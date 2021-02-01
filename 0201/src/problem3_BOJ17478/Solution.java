2461d285345d9cd5027cb0a70b3861dda30d84b6
이종현 <dlwhdgus53@naver.com>
2021-02-01 20:16:18 +0900

    0201 homework

--- 0201/src/problem3_BOJ17478/Solution.java
+++ 0201/src/problem3_BOJ17478/Solution.java
@@ -17,14 +17,6 @@ public class Solution {
 	public static void chatBot(int n) {
 		underbarAdd(underbarCnt);
 		System.out.println("\"재귀함수가 뭔가요?\"");
-		
-		underbarAdd(underbarCnt);
-		System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-		underbarAdd(underbarCnt);
-		System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		underbarAdd(underbarCnt);
-		System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-		underbarCnt++;
 		if(n==0) {
 			underbarAdd(underbarCnt);
 			System.out.println("\"재귀함수는 자기 자신을 호출하는 함수라네\"");			
@@ -35,6 +27,15 @@ public class Solution {
 			}
 			return;
 		}
+		
+		underbarAdd(underbarCnt);
+		System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+		underbarAdd(underbarCnt);
+		System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+		underbarAdd(underbarCnt);
+		System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		underbarCnt++;
+		
 		chatBot(n-1);
 	}
 	
